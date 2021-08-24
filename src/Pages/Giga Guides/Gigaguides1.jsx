bceb6074f1950808cfd52c08f62fd93c9517c291
Zachary MacTavish <zacharymactavish@Zacharys-MacBook-Pro.local>
2021-08-24 12:36:53 -0700

    added more sections to giga guides - create assessment

--- src/Pages/Giga Guides/Gigaguides1.jsx
+++ src/Pages/Giga Guides/Gigaguides1.jsx
@@ -2,7 +2,14 @@
 import styled from 'styled-components';
 import { ButtonOne } from '../../Components/Buttons/Button.styles';
 import web1 from '../Giga Guides/Web-Group.png';
+import web2 from '../Giga Guides/Assessment-Section.png';
+import web3 from '../Giga Guides/Toggle-Topics.png';
 import { BigTexthome, Paragraph } from '../Homepage2';
+import { TrendingNowContainer } from '../../Components/Trending Now Section/Trendingnow.styles';
+import { FlexTextbutton, TextGetstarted } from '../../Components/Get Started/Getstarted.styles';
+import {Link} from 'react-router-dom';
+import rubric from '../Giga Guides/Rubric-Photo.png';
+import { MindsConnect } from '../../Components/Minds Connect Section/Mindsconnect.styles';
 
 const GigaGuideone = styled.div`
 display: flex;
@@ -12,11 +19,18 @@ align-items: center;
 `
 
 const WebImage = styled.img`
-
 height: 80vh;
-
 padding-left: 3vw;
+`
 
+const WebImagetwo = styled.img`
+height: 60vh;
+padding-left: 7vw;
+padding-right: 11vw;
+`
+export const RubricImage = styled.img`
+margin-left: 4vw;
+height: 50vh;
 
 
 `
@@ -24,12 +38,12 @@ padding-left: 3vw;
 
 function GigaGuidesone() {
     return (
+      <div>
+
+          {/* Web Photo Section */}
        <GigaGuideone>
       
       <WebImage src={web1 }/>
-
-
-      
 
 <div>
 <BigTexthome>Create Assessment on a Subject or Key Factor in a Category </BigTexthome>
@@ -40,7 +54,54 @@ function GigaGuidesone() {
 </div>
 
 </GigaGuideone>
-      
+
+ {/* Assessment Section */}
+  <GigaGuideone>
+    <WebImagetwo src={web2 }/>
+    <div>
+    <BigTexthome>The Assessment</BigTexthome>
+    <Paragraph>The assessment of each topic consists of a rating, narrative, images, and attached files.
+      This is an example of filling out one of the assessment topics. Allowing you to write a robust reponse.
+    </Paragraph>
+    <ButtonOne Backgroundcolor="rgba(0,0,0, 0.5)">Create Assessment</ButtonOne>
+    </div>
+</GigaGuideone>
+
+ {/* Assessment Section */}
+ <TrendingNowContainer>
+                    <MindsConnect>
+                    <FlexTextbutton>
+                          <BigTexthome Backgroundcolor="#2ca3ff">Our Rubric.</BigTexthome>
+                          <Paragraph>The Giga system’s rubric to analyze a subject includes the Future, Context, Importance, Wisdom, Knowledge, Perspective, Current, and Outcome. 
+                            See our FAQ below to see specific examples!   </Paragraph>
+                                {/* Link to Learn More */}
+                            <Link to="/learnmore">
+                                    <ButtonOne Backgroundcolor="rgba(0,0,0, 0.3)">Create Assessment</ButtonOne>
+                            </Link>
+
+                        </FlexTextbutton>
+                        <div>
+                    <RubricImage  src={rubric} />
+                        </div>
+
+                    </MindsConnect>
+
+
+                  </TrendingNowContainer>
+
+ {/* Toggle Topics */}
+ <GigaGuideone>
+    <WebImagetwo src={web3 }/>
+    <div>
+    <BigTexthome>The Assessment</BigTexthome>
+    <Paragraph>When analyzing a subject, the Giga system is flexible to toggle rubric topics.
+      Start your basic membership now, and share your viewpoints with the world.
+    </Paragraph>
+    <ButtonOne Backgroundcolor="rgba(0,0,0, 0.5)">Toggle on Topics</ButtonOne>
+    </div>
+</GigaGuideone>
+
+</div>    
       
     );
   }
